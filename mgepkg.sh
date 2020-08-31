58378a185481acf2927db52d621b6dc4a7a4b23f
Bhaviktutorials <64035221+Bhaviktutorials@users.noreply.github.com>
2020-08-31 12:15:44 +0530

    u

---
+++ mgepkg.sh
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#Downloading the req dependencies to run a script without error
+apt-get update -yq --silent
+apt-get upgrade -y
+apt-get install python -yq --silent
+apt-get install ruby -y
+pip install install lolcat
+gem install lolcat 
+
+mv mgk /data/data/com.termux/files/usr/bin
+echo
+
+echo -en "\e[93m Do you wish to see a practical video on it (y/n)? \e[m "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] 
+then
+am start -a android.intent.action.VIEW -d https://www.youtube.com/watch?v=JCIfMsObDv8 
+ cd $HOME 
+ rm -rf Termux-Megapackage
+else
+    echo 
+    exit
+fi
+
+cd $HOME
+rm -rf Termux-Megapackage
+echo
+
+
