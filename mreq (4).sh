75aff58917d2a657dc0804ea88dd1ff7a05b2b76
Bhaviktutorials <64035221+Bhaviktutorials@users.noreply.github.com>
2020-08-31 21:38:02 +0530

    Delete mreq (4).sh

--- mreq (4).sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-apt-get update -yq --silent
-apt-get upgrade -y
-apt-get install python -yq --silent
-apt-get install ruby -y
-pip install install lolcat
-gem install lolcat 
